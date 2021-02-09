f03475e8539b499472528054c78b27a9d3b881d8
Ilya Nikolsky <ilya.nikolsky@flexdsl.ch>
2021-02-09 15:56:05 +0100

    Made homework 05

--- 05/src/lib.cpp
+++ 05/src/lib.cpp
@@ -25,4 +25,45 @@ namespace MyLib {
 			*(array + i) = i * 3 + 1;
 		printArray(array, size);
 	}
+
+	bool checkBalance(int *array, int *size) {
+		int lsum = 0;
+		int rsum;
+		int i = 0;
+		do {
+			lsum += *(array + i);
+			rsum = 0;
+			for (int j = i + 1; j < *size; j++) 
+				rsum += *(array + j);
+			i++;
+		} while (lsum != rsum && i < *size);
+		return (lsum == rsum);
+	}
+
+	void shiftArray(int *array, int *size, int *offset) {
+		if (*offset == 0)
+			return;
+
+		if (*offset > 0) {
+			int i = *size;
+			int tmp = *(array + --i);
+			while (i) {
+				*(array + i) = *(array + (i - 1));
+				i--;
+			}
+			*array = tmp;
+			--(*offset);
+			shiftArray(array, size, offset);
+		} else {
+			int i = 0;
+			int tmp = *(array);
+			while (i < *size) {
+				*(array + i) = *(array + (i + 1));
+				i++;
+			}
+			*(array + *size - 1) = tmp;
+			++(*offset);
+			shiftArray(array, size, offset);
+		}
+	}
 }
--- 05/src/main.cpp
+++ 05/src/main.cpp
@@ -6,12 +6,31 @@ int main(int argc, char **args) {
 	// Task 5
 	int arsize = 10;
 	int array[arsize] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+	std::cout << "Task 5, reverce the array" << std::endl;
 	reverseArray(array, &arsize);
 	
 	// Task 2
 	arsize = 8;
 	int emptyarray[arsize] = {0};
+	std::cout << "Array from Task 2:" << std::endl;
 	changeArray(emptyarray, &arsize);
-	
+
+	// Task 3 + 6
+	arsize = 10;
+	int balancearray[arsize] = {10, 0, 1, 0, 2, 0, 3, 0, 4, 0};
+	bool (*p_cB) (int*, int*);
+	p_cB = checkBalance;
+	std::cout << "Task 3, check if array is balanced" << std::endl;
+	printArray(balancearray, &arsize);
+	(*p_cB)(balancearray, &arsize) ? std::cout << "The array is balanced" << std::endl : std::cout << "The arryy is not balanced" << std::endl;
+
+	// Task 4
+	arsize = 10;
+	int offset = -3;
+	int cyclearray[arsize] = {3, 2, 1, 4, 7, 6, 5, 9, 0, 8};
+	std::cout << "Task 4, move values inside the array" << std::endl;
+	printArray(cyclearray, &arsize);
+	shiftArray(cyclearray, &arsize, &offset);
+	printArray(cyclearray, &arsize);
 	return SUCCESS;
 }
